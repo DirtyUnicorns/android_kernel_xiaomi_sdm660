bdb88f36218e3b35106d1ef268b2312ef36a6780
Khusika Dhamar Gusti <khusikadhamar@gmail.com>
2020-08-23 18:03:29 -0400

    input: touchscreen: nt36xxx: fix snprintf warning
    Fixes the following clang warning:
    
    ../drivers/input/touchscreen/nt36xxx/nt36xxx_mp_ctrlram.c:1293:3: error: 'snprintf' size argument is too large; destination buffer has size 32, $
                    snprintf(mpcriteria, PAGE_SIZE, "novatek-mp-criteria-%04X", ts->nvt_pid);
                    ^
    1 error generated.
    
    Change-Id: Ieb3331a226cb6e183aaac2f22e7e8dccf27af7fc
    Signed-off-by: Khusika Dhamar Gusti <khusikadhamar@gmail.com>

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx_mp_ctrlram.c
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx_mp_ctrlram.c
@@ -1415,7 +1415,7 @@ static ssize_t nvt_test_result_read(struct file *file,char __user *buf, size_t s
 {
 	int cnt = 0;
 	char *page = NULL;
-	
+
 	page = kzalloc(16, GFP_KERNEL);
 	NVT_LOG("test_result have %d err",test_result);
 
@@ -1484,7 +1484,7 @@ static int32_t nvt_selftest_open(struct inode *inode, struct file *file)
 		 * Ex. nvt_pid = 500A
 		 *     mpcriteria = "novatek-mp-criteria-500A"
 		 */
-		snprintf(mpcriteria, PAGE_SIZE, "novatek-mp-criteria-%04X", ts->nvt_pid);
+		snprintf(mpcriteria, sizeof(mpcriteria), "novatek-mp-criteria-%04X", ts->nvt_pid);
 
 		nvt_mp_parse_dt(np, mpcriteria);
 	} else {
